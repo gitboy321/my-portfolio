ea08cd9ec9cea2ee1de52815059e1428b935334c
Bharath <kumarbharatvgsb321@gmail.com>
2026-04-20 09:54:01 +0530

    fix paths, search icon, tabs working

--- script.js
+++ script.js
@@ -117,3 +117,20 @@ window.addEventListener('scroll', () => {
 backToTop.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".section");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  }, { threshold: 0.1 });
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+});
